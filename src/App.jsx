5023212e638ee4a4a32ba8ffdf68e77d5a483e56
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2024-05-28 17:33:52 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
 import Footer from './components/Footer';
+import * as Sentry from '@sentry/react';
 
 const App = () => {
+  // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
 
   return (
     <main className="bg-black">
@@ -21,4 +23,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
