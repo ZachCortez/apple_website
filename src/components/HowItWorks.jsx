9c3e2cae8d55c4a3c5f9f419779559c2330dab42
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2024-05-31 22:47:46 -0700

    Add files via upload

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,7 +1,22 @@
 
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
+    <section className="common-padding">
+        <div id="chip" className="flex-center w-full my-20">
+          <img  />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h2 className="hiw-title">
+            A17 Pro chip.
+            <br /> A monster win for gaming.
+          </h2>
+
+          <p className="hiw-subtitle">
+            It's here. The biggest redesign in the history of Apple GPUs.
+          </p>
+        </div>
+    </section>
   )
 }
 
