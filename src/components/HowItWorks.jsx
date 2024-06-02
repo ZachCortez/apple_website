dff0f75f98732e5005e813d6b910bbaa4b974aae
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2024-06-01 21:11:00 -0700

    Add files via upload

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -2,8 +2,9 @@
 const HowItWorks = () => {
   return (
     <section className="common-padding">
+        How It Works.
         <div id="chip" className="flex-center w-full my-20">
-          <img  />
+          <img src={chipImg} alt="chip" width={180} height={180} />
         </div>
 
         <div className="flex flex-col items-center">
@@ -16,6 +17,23 @@ const HowItWorks = () => {
             It's here. The biggest redesign in the history of Apple GPUs.
           </p>
         </div>
+
+        <div className="flex flex-1 justify-center flex-col">
+                  <p className="hiw-text g_fadeIn">
+                    A17 Pro is an entirely new class of iPhone chip that delivers our {' '}
+                    <span className="text-white">
+                      best graphic performance by far
+                    </span>.
+                  </p>
+
+                  <p className="hiw-text g_fadeIn">
+                   Mobile {' '}
+                    <span className="text-white">
+                      games will look and feel so immersive
+                    </span>,
+                     with incredibly detailed environments and characters.
+                  </p>
+                </div>
     </section>
   )
 }
